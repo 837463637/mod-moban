baf8c6a977b2e25295456c1fe6aa5c5d6eefe085
userName <userEmail>
2024-11-17 12:04:32 +0800

    Initial commit

---
+++ Name-MOD/src/main/java/dideng/name/modid/NameClient.java
@@ -0,0 +1,10 @@
+package dideng.name.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NameClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ Name-MOD/src/main/java/dideng/name/modid/NameDataGenerator.java
@@ -0,0 +1,11 @@
+package dideng.name.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NameDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
